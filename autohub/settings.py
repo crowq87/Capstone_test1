a90b7df3c5aabce1cba5926b538e18f0d296dde5
Boyles, Prince Niño <oraora871@gmail.com>
2026-07-18 15:09:35 +0800

    Change ALLOWED_HOSTS to use os.getenv

--- autohub/settings.py
+++ autohub/settings.py
@@ -18,7 +18,10 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-autohub-change-this-i
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
